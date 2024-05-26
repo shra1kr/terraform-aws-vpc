2d0be31e96b9a66ee10b0c39ca08fd723e150766
shravan <shravankumar6262@gmail.com>
2024-05-26 16:41:31 +0530

    committed by shravan

--- output.tf
+++ output.tf
@@ -6,17 +6,17 @@ output "vpc_id" {
     value = aws_vpc.main.id
 }
 
-output "public_sbnet_ids" {
+output "public_subnet_ids" {
     value = aws_subnet.public[*].id
 
 }
 
-output "private_sbnet_ids" {
+output "private_subnet_ids" {
     value = aws_subnet.private[*].id
 
 }
 
-output "database_sbnet_ids" {
+output "database_subnet_ids" {
     value = aws_subnet.database[*].id
 
 }
@@ -28,3 +28,35 @@ output "database_subnet_group_id" {
 output "igw_id" {
     value = aws_internet_gateway.gw.id
 }
+
+# output "azs" {
+#     value = data.aws_availability_zones.available.names
+# }
+
+# output "vpc_id" {
+#   value = aws_vpc.main.id
+# }
+
+# output "public_subnet_ids" {
+#   value = aws_subnet.public[*].id
+# }
+
+# output "private_subnet_ids" {
+#   value = aws_subnet.private[*].id
+# }
+
+# output "database_subnet_ids" {
+#   value = aws_subnet.database[*].id
+# }
+
+# output "database_subnet_group_id" {
+#   value = aws_db_subnet_group.default.id
+# }
+
+# output "database_subnet_group_name" {
+#   value = aws_db_subnet_group.default.name
+# }
+
+# output "igw_id" {
+#   value = aws_internet_gateway.gw.id
+# }
