0b8f3cf9b588e5183fd6c65c4b98b90292a457d1
shravan <shravankumar6262@gmail.com>
2024-05-26 17:48:24 +0530

    committed by shravan

--- vpc.tf
+++ vpc.tf
@@ -155,7 +155,7 @@ resource "aws_route_table" "database" {
 }
 
 ### public route ###
-resource "aws_route" "public_route" {
+resource "aws_route" "public_route_gw" {
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.gw.id
